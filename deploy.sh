790033d625fdb47b0b378b75b26606f180a6417c
dong4j <dong4j@gmail.com>
2025-01-18 15:37:22 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,30 @@ fi
 REMOTE_HOST="$1"
 REMOTE_DIR="$2"
 
+# 检查并创建远程目录
+ssh "$REMOTE_HOST" "mkdir -p '$REMOTE_DIR'"
+if [ $? -ne 0 ]; then
+  echo "无法创建远程目录，请检查连接或权限配置。"
+  exit 1
+fi
+
 # 定义本地目录
 LOCAL_DIR="dist"
 
+# 检查目录是否存在
+if [ ! -d "$LOCAL_DIR" ]; then
+  echo "$LOCAL_DIR 目录不存在，正在执行 bun run build 以生成最新的文件..."
+  bun run build
+
+  # 再次检查目录是否生成成功
+  if [ ! -d "$LOCAL_DIR" ]; then
+    echo "$LOCAL_DIR 目录生成失败！"
+    exit 1
+  fi
+fi
+
 # 上传文件到远程并覆盖
-echo "正在上传 dist 目录下的所有文件到 $REMOTE_HOST:$REMOTE_DIR..."
+echo "正在上传 $LOCAL_DIR 目录下的所有文件到 $REMOTE_HOST:$REMOTE_DIR..."
 rsync -azqhP --delete \
   --exclude '.DS_Store' \
   --exclude '._*' \
